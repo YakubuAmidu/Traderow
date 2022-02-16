ae39db3254b0479ceab35d5e6b3b8ad9f03b16cc
Yakubu <yakubuamidu19@gmail.com>
2022-02-15 21:17:35 -0800

    Initial commit

--- front-traderow/Screens/Products/ProductContainer.js
+++ front-traderow/Screens/Products/ProductContainer.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+
+import ProductList from './ProductList';
+
+const data = require('../../assets/data/product.json');
 
 const ProductContainer = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(data);
+
+        return () => {
+            setProducts([]);
+        }
+    }, []);
+
     return (
         <View>
             <Text>ProductContainer</Text>
+            <View style={{ marginTop: 100 }}>
+            <FlatList 
+            // numColumns={2}
+            horizontal
+             data={products}
+             renderItem={({ item }) => <ProductList key={item.id} item={item}/>}
+             keyExtractor={item => item.name}
+            />
+            </View>
         </View>
     );
 }
