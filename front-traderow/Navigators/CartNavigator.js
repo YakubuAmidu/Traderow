548d474cba00abcec95baaff895e7168d36f2502
Yakubu <yakubuamidu19@gmail.com>
2022-02-26 11:02:05 -0800

    Initial commit

--- front-traderow/Navigators/CartNavigator.js
+++ front-traderow/Navigators/CartNavigator.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Stack } from "native-base";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { createStackNavigator } from "react-navigation/stack";
 
-const CartNavigator = () => {
-    return (
-        <View>
-            <Text>CartNavigator</Text>
-        </View>
-    )
+import Cart from "../Screens/Cart/Cart";
+import CheckoutNavigator from "./CheckoutNavigator";
+
+const Tab = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Tab.Navigaor>
+      <Stack.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="Checkout"
+        component={CheckoutNavigator}
+        options={{
+          title: "Checkout",
+        }}
+      />
+    </Tab.Navigaor>
+  );
 }
 
-const styles = StyleSheet.create({
-
-});
+export default function CartNavigator(){
+    return (
+        <MyStack />
+    )
+}
 
 export default CartNavigator;
