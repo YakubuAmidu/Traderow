d9ea3c2b65206af3bb82e7202b713f2c7ed2237e
Yakubu <yakubuamidu19@gmail.com>
2022-02-27 23:07:36 -0800

    Inial commit

--- front-traderow/Screens/Cart/Checkout/Confirm.js
+++ front-traderow/Screens/Cart/Checkout/Confirm.js
@@ -7,11 +7,26 @@ import * as actions from "../../../Redux/Actions/CartActions";
 
 var { height } = Dimension.get("window");
 
-const Confirm = () => {
+const Confirm = (props) => {
+  const confirm = props.route.params;
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>Confirm order</Text>
+
+        {props.route.params ? (
+          <View style={{ borderWidth: 1, borderColor: "orange" }}>
+            <Text style={styles.shipping}>Shipping to:</Text>
+            <View style={{ padding: 8 }}>
+              <Text>Address: {confirm.order.order.shippingaddress1}</Text>
+              <Text>Address2: {confirm.order.order.shipppingaddress2}</Text>
+              <Text>City: {confirm.order.order.city}</Text>
+              <Text>Zip: {confirm.order.order.zip}</Text>
+              <Text>Country: {confirm.order.order.country}</Text>
+            </View>
+          </View>
+        ) : null}
       </View>
     </ScrollView>
   );
@@ -23,6 +38,17 @@ const styles = StyleSheet.create({
     padding: 8,
     alignContent: "center",
     backgroundColor: "white",
+  },
+  titleContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 8,
+  },
+  shipping: {
+    alignSelf: "center",
+    margin: 8,
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
