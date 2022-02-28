33bf6d4e0177ad0f4231a44a51f545a90afd64b0
Yakubu <yakubuamidu19@gmail.com>
2022-02-27 22:51:31 -0800

    Initial commit

--- front-traderow/Screens/Cart/Checkout/Confirm.js
+++ front-traderow/Screens/Cart/Checkout/Confirm.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Dimension, ScrollView } from "react-native";
+import { Text, Left, Right, ListItem, Body, Thumbnail } from "native-base";
+
+import { connect } from "react-redux";
+import * as actions from "../../../Redux/Actions/CartActions";
+
+var { height } = Dimension.get("window");
 
 const Confirm = () => {
   return (
-    <View>
-      <Text>Confirm</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Confirm order</Text>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: height,
+    padding: 8,
+    alignContent: "center",
+    backgroundColor: "white",
+  },
+});
 
 export default Confirm;
