76c9a6159e9f13befb73499d8160bb53575bd0e4
Yakubu <yakubuamidu19@gmail.com>
2022-03-03 10:12:27 -0800

    Initial commit

--- front-traderow/Screens/Admin/Products.js
+++ front-traderow/Screens/Admin/Products.js
@@ -1,5 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { Header, Icon, Input } from "native-base";
+import { useFocusEffect } from "@react-navigation/native";
+
+import axios from "axios";
+import baseURL from "../../assets/common/baseUrl";
+import AsyncStorage from "@react-native-community/async-storage";
+
+var { height, width } = Dimensions.get("window");
 
 const Products = (propw) => {
   return (
