bd72b108f77d25661002baf022666e245cd659f3
Yakubu <yakubuamidu19@gmail.com>
2022-03-03 05:09:54 -0800

    Initial commit

---
+++ front-traderow/Screens/Admin/Orders.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const Orders = (props) => {
+  return (
+    <View>
+      <Text>Orders</Text>
+    </View>
+  );
+};
+
+export default Orders;
---
+++ front-traderow/Screens/Admin/ProductForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const ProductsForm = (props) => {
+  return (
+    <View>
+      <Text>Product Form</Text>
+    </View>
+  );
+};
+
+export default ProductsForm;
