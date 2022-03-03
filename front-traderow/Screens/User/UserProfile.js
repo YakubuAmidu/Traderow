0aca8d76962a9b13941e19a71e57b86068bb6205
Yakubu <yakubuamidu19@gmail.com>
2022-03-03 04:39:27 -0800

    Initial commit

--- front-traderow/Screens/User/UserProfile.js
+++ front-traderow/Screens/User/UserProfile.js
@@ -1,7 +1,38 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect, useContext, useCallback } from "react";
+import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
+import { Container } from "native-base";
+import { useFocusEffect } from "@react-navigation/native";
+import AsynStorage from "@react-native-community/async-storage";
+
+import axios from "axios";
+import { baseURL } from "../../assets/common/baseUrl";
+
+import AuthGlobal from "../../Context/Store/AuthGlobal";
+import { logoutUser } from "../../Context/Actions/Auth.actions";
 
 const UserProfile = () => {
+  const context = useContext(AuthGlobal);
+  const [userProfile, setUserProfile] = useState();
+
+  useEffect(() => {
+    if (
+      context.stateUser.isAuthenticated === false ||
+      context.stateUser.isAuthenticated === null
+    ) {
+      props.navigation.navigate("Login");
+    }
+
+    AsynStorage.getItem("jwt")
+      .then((res) => {
+        axios
+          .get(`${baseURL}users/${context.stateUser.user.sub}`, {
+            headers: { Authorizatin: `Bearer ${res}` },
+          })
+          .then((user) => setUserProfile(user.data));
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <View>
       <Text>UserProfile</Text>
