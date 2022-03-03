ca6e2411680cffbeb1ccfda1db1856a4b583a610
Yakubu <yakubuamidu19@gmail.com>
2022-03-03 10:23:46 -0800

    Initial commit

--- front-traderow/Screens/Admin/Products.js
+++ front-traderow/Screens/Admin/Products.js
@@ -17,6 +17,30 @@ import AsyncStorage from "@react-native-community/async-storage";
 var { height, width } = Dimensions.get("window");
 
 const Products = (propw) => {
+  const [productList, setProductList] = useState();
+  const [productFilter, setProductFilter] = useState();
+  const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState();
+
+  useFocusEffect(() => {
+    //Get Token
+    AsyncStorage.getItem("jwt")
+      .then((res) => setToken(res))
+      .catch((err) => console.log(err));
+
+    axios.get(`${baseURL}products`).then((res) => {
+      setProductList(res.data);
+      setProductFilter(res.data);
+      setLoading(false);
+    });
+
+    return () => {
+      setProductList();
+      setProductFilter();
+      setLoading(true);
+    };
+  }, []);
+
   return (
     <View>
       <Text>Products Screen</Text>
