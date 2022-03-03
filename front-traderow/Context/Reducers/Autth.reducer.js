7edb4aac660652ea985fb9caa692d10d1afa2bce
Yakubu <yakubuamidu19@gmail.com>
2022-03-02 22:19:15 -0800

    Initial commit

--- front-traderow/Context/Reducers/Autth.reducer.js
+++ front-traderow/Context/Reducers/Autth.reducer.js
@@ -1,11 +1,12 @@
 import { SET_CURRENT_USER } from "../Actions/Auth.actions";
+import isEmpty from "../../assets/common/Is-empty";
 
 export default function (state, action) {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
         ...state,
-        iseAuthenticated: [], // TODO
+        iseAuthenticated: !isEmpty(action.payload), // TODO
         user: action.payload,
         userProfile: action.userProfile,
       };
