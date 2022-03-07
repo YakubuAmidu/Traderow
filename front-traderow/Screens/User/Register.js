47ba5e9959f37bafedbc45e3811b0bccc214facc
Yakubu <yakubuamidu19@gmail.com>
2022-03-07 00:28:33 -0800

    Initial commit

--- front-traderow/Screens/User/Register.js
+++ front-traderow/Screens/User/Register.js
@@ -4,6 +4,7 @@ import FormContainer from "../../Shared/Form/FormContainer";
 import Input from "../../Shared/Form/Input";
 import Toast from "react-native-toast-message";
 import { keyBoardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import EasyButton from "../../Shared/StyledComponents/EasyButton";
 import Error from "../../Shared/Error";
 
 import axios from "axios";
@@ -101,14 +102,19 @@ const Register = () => {
           {error ? <Error message={error} /> : null}
         </View>
         <View>
-          <Button title={"Register"} onPress={() => register()} />
+          <EasyButton large primary onPress={() => register()}>
+            <Text style={{ color: "white" }}>Register</Text>
+          </EasyButton>
         </View>
 
         <View>
-          <Button
-            title={"Back to login"}
+          <EasyButton
+            large
+            secondary
             onPress={() => props.navigation.navigate("Login")}
-          />
+          >
+            <Text style={{ color: "white" }}>Back to login</Text>
+          </EasyButton>
         </View>
       </FormContainer>
     </keyBoardAwareScrollView>
