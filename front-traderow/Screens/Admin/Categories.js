f72f94609bef6e3cce0ac07e5871fc35c2bf4b35
Yakubu <yakubuamidu19@gmail.com>
2022-03-09 05:16:16 -0800

    Initial commit

--- front-traderow/Screens/Admin/Categories.js
+++ front-traderow/Screens/Admin/Categories.js
@@ -1,12 +1,119 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  TextInput,
+} from "react-native";
+import EasyButton from "../../Shared/StyledComponents/EasyButton";
+import baseURL from "../../assets/common/baseUrl";
+import axios from "axios";
+import AsyncStorage from "@react-native-community/async-storage";
 
-const Categories = (props) => {
+const { width } = Dimensions.get("window");
+
+const Item = (props) => {
   return (
-    <View>
-      <Text>Categories Screen</Text>
+    <View style={styles.item}>
+      <Text>{props.item.name}</Text>
+      <EasyButton medium danger>
+        <Text style={{ color: "white", fontWeight: "bold" }}>Delete</Text>
+      </EasyButton>
     </View>
   );
 };
+
+const Categories = (props) => {
+  const [categories, setCategories] = useState([]);
+  const [categoryName, setCategoryName] = useState();
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    AsyncStorage.getItem("jwt")
+      .then((res) => setToken(res))
+      .catch((error) => console.log(error));
+
+    axios
+      .get(`${baseURL}categories`)
+      .then((res) => setCategories(res.data))
+      .catch((error) => alert("Error to load category..."));
+
+    return () => {
+      setCategories();
+      setToken();
+    };
+  }, []);
+
+  return (
+    <View style={{ position: "relative", height: "100%" }}>
+      <View style={{ marginBottom: 60 }}>
+        <FlatList
+          data={categories}
+          renderItem={({ item, index }) => <Item item={item} index={index} />}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+
+      <View style={styles.bottomBar}>
+        <View>
+          <Text>Add category</Text>
+        </View>
+
+        <View style={{ width: width / 2.5 }}>
+          <TextInput
+            style={styles.input}
+            value={categoryName}
+            onChangeText={(text) => setCategoryName(text)}
+          />
+        </View>
+
+        <View>
+          <EasyButton medium primary>
+            <Text style={{ color: "white", fontWeight: "bold" }}>Submit</Text>
+          </EasyButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomBar: {
+    backgroundColor: "white",
+    width: width,
+    height: 60,
+    padding: 2,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+  },
+  input: {
+    height: 40,
+    borderColor: "grey",
+    borderWidth: 1,
+  },
+  item: {
+    showColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+    elevation: 1,
+    padding: 5,
+    margin: 5,
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderRadius: 5,
+  },
+});
 
 export default Categories;
