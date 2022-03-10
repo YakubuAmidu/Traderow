35e4eb528fd5f54665a48d0b92c3ee04e3875c1f
Yakubu <yakubuamidu19@gmail.com>
2022-03-10 12:18:30 -0800

    Initial commit

--- front-traderow/Screens/Admin/Orders.js
+++ front-traderow/Screens/Admin/Orders.js
@@ -1,10 +1,38 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState, useCallback } from "react";
+import { FlatList, View, Text } from "react-native";
+import baseURL from "../../assets/common/baseUrl";
+import axios from "axios";
+import { useFocusEffect } from "@react-navigation/native";
 
 const Orders = (props) => {
+  const [orderListis, setOrderList] = useState();
+
+  useFocusEffect(
+    useCallback(() => {
+      getOrders();
+
+      return () => {
+        setOrderList();
+      };
+    }, [])
+  );
+
+  const getOrders = () => {
+    axios
+      .get(`${baseURL}orders`)
+      .then((x) => setOrderList(x.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <View>
-      <Text>Orders</Text>
+      <FlatList
+        data={orderListis}
+        renderItem={({ item }) => {
+          <Text>{item.shippingAddress1}</Text>;
+        }}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };
